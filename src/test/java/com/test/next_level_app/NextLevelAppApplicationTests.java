a14ecb09f61f7c51715ae6e67fb87d9bb52f4a7a
LariKubisse <larikubisse@hotmail.com>
2025-03-30 20:18:06 -0300

    Iniciando app bank

---
+++ src/test/java/com/test/next_level_app/NextLevelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.next_level_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextLevelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
